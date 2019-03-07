cd8c610be63c8495d0a94a4a078389625e165d47
Laura Hoffman <lhoffmanwg@gmail.com>
2019-03-07 20:57:35 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -15,3 +15,9 @@ def select_winner(passengers)
   end 
 winner 
 end
+
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |name, age|
+    puts "Happy Birthday #{name}! You are now #{age} years old!"  
+  end
+end  
